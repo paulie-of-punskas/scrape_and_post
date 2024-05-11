e9a38a3221a904b87124f5b6bfb4aa51ec1290f2
paulie-of-punskas <pbernecki@protonmail.com>
2024-05-11 14:07:10 +0200

    minor updates

--- scrape_and_post/views.py
+++ scrape_and_post/views.py
@@ -3,17 +3,16 @@ from scrape_and_post.src.analyze_data.output_scraped_data import output_scraped_
 from scrape_and_post.src.scrape_data.run_scrape_codes import run_scrape_codes
 from flask import make_response, render_template_string
 import pandas
+from datetime import datetime
 
 import os
 
-# a simple page that says hello
-@app.route('/hello')
-def hello():
-    return 'Hello, World!'
-    
+
 @app.route('/')
 def hello_world():
-    return "Laba diena."
+    scrape_datetime = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    print(scrape_datetime)
+    return f"Laba diena, siandien yra {scrape_datetime}"
 
 # run scraping codes
 @app.route('/scrape_new_data')
