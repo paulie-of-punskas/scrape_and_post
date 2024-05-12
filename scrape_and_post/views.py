443a5d5b5226a7bfb391e6eff22bb2eff8c0fdee
paulie-of-punskas <pbernecki@protonmail.com>
2024-05-12 12:35:00 +0200

    updated templates; endpoints

--- scrape_and_post/views.py
+++ scrape_and_post/views.py
@@ -1,7 +1,7 @@
 from scrape_and_post import app
 from scrape_and_post.src.analyze_data.output_scraped_data import output_scraped_data
 from scrape_and_post.src.scrape_data.run_scrape_codes import run_scrape_codes
-from flask import make_response, render_template
+from flask import make_response, render_template, url_for
 import pandas
 from datetime import datetime
 import os
@@ -11,13 +11,14 @@ import os
 def index():
     current_datetime = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     df = output_scraped_data()
-    # === convert pandas dataframe to list -- this will later be used for printing df values to html
-    df_as_list = list(df.values.tolist())
 
     # === check if all price_lowered values are "True"/"False"
-    print(df["price_lowered"].any() == True)
+    if (df["price_lowered"].any() == True):
+        any_price_lowered = True
+    else:
+        any_price_lowered = False
 
-    return render_template("index.html", current_datetime=current_datetime, df=df, df_as_list=df_as_list, zip=zip)
+    return render_template("index.html", current_datetime=current_datetime, any_price_lowered=any_price_lowered)
     # return render_template("index.html", scrape_datetime=scrape_datetime, df_html=df_html)
 
 # run scraping codes
@@ -26,11 +27,18 @@ def scrape_results():
     run_scrape_codes()
     return "New data was scraped."
     
-@app.route('/view_scraped_data')
+@app.route('/view_scraped_data_old')
 def show_scrape_results():
     df_html = output_scraped_data().to_html()
     # resp = make_response(render_template_string(df_html))
     return df_html
+
+@app.route('/view_scraped_data')
+def show_scrape_results2():
+    df = output_scraped_data()
+    # === convert pandas dataframe to list -- this will later be used for printing df values to html
+    df_as_list = list(df.values.tolist())
+    return render_template("view_scraped_data.html", df=df, df_as_list=df_as_list, zip=zip)
 
 @app.route('/csv_exists')
 def csv_exists():
